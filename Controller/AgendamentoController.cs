1f8da7427c134b15739fd46e72c0b51164df1281
MayconL27 <mayconleandro2008@gmail.com>
2024-06-25 21:17:11 -0300

    Aula 5

---
+++ Controller/AgendamentoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consultorio.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+       [HttpGet]
+       public IActionResult Get()
+        {
+            return Ok("Minha primeira requisição");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Consultorio.Controller;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace Consultorio
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
